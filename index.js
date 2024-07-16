27a803df1b905dcc36d21dab0087656179e8b8c7
r4andom1 <kim.petersson301@gmail.com>
2024-07-16 20:40:11 +0200

    update main

--- index.js
+++ index.js
@@ -4,4 +4,37 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
      "+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
 ];
 
+function genPassword() {
+    // Generates password for button
+    let randomChar = characters[genRandomNumbers(characters)]
+    for (i = 0; i < nrOfChars; i++) {
+        password.push(randomChar)
+    }
+    console.log(password)
+}
+
+function iterateList(list) {
+    // Iterates over list of characters
+    for (let i = 0; i < list.length; i++) {
+        password += list[i]
+    }
+    return password
+}
+
+function genRandomNumbers(list) {
+    // Returns random number based of list length
+    let listLen = list.length;
+    return Math.floor(Math.random() * listLen)
+}
+let nrOfChars = 12; 
+let password = [];
+
+genPassword()
+
+// 1. Have user input how long their password should be.
+// 2. Generate random numbers depending on how long their password is. Store in list?
+// 3. Pick out random characters from characters[] based on generated random numbers.
+// 4. Add the random characters to a password variable.
+// 5. 
+// Optional: Have them choose to include symbols/numbers
 
