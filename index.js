57482b2c80bde810f8c5c183967466a31b4b3380
r4andom1 <kim.petersson301@gmail.com>
2024-07-17 12:22:43 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function genPassword() {
+function genPassword(list) {
     // Generates password for button
-    if (! passwordDone) {
-        for (i = 0; i < nrOfChars; i++) {
-            let randomChar = characters[genRandomNumbers(characters)]; // makes random character
-            password += randomChar;
-        }
-        passwordDone = true;
-        passwordEl.textContent = "New Password: " + password;
+    let password = "";
+    for (i = 0; i < nrOfChars; i++) {
+        let randomChar = list[genRandomNumbers(list)]; // makes random character
+        password += randomChar;
     }
-    
+    return password
+    //passwordEl.textContent = "New Password: " + password;
 }
 
 function genRandomNumbers(list) {
@@ -17,22 +15,51 @@ function genRandomNumbers(list) {
     return Math.floor(Math.random() * listLen)
 }
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-",
-     "+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+const letters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
 ];
 
-let nrOfChars = 12; 
-let password = [];
-let passwordDone = false;
-let passwordEl = document.getElementById("newpassword-el");
-let checkbox = document.querySelector("#letters");
+const symbols = [
+    "~","`","!","@","#","$","%","^","&","*","(",")","_","-",
+    "+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+];
 
-// 1. Have user input how long their password should be.
-// 2. Generate random numbers depending on how long their password is. Store in list?
-// 3. Pick out random characters from characters[] based on generated random numbers.
-// 4. Add the random characters to a password variable.
+const numbers = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
+
+let nrOfChars = 12; // How long password should be
+let passwordEl = document.getElementById("newpassword-el");
+let checkboxLet = document.querySelector("#letters");
+let checkboxNum = document.querySelector("#numbers");
+let checkboxSym = document.querySelector("#symbols");
+
+function outputPassword() {
+    if (checkboxLet.checked && checkboxNum.checked && checkboxSym.checked) {
+        let letNumSym = letters.concat(numbers, symbols); // concetenates letters, numbers, symbols lists
+        passwordEl.textContent = "New Password: " + genPassword(letNumSym);
+        console.log("letNumSym checked")
+    } else if (checkboxLet.checked && checkboxNum.checked) {
+        let letNum = letters.concat(numbers); // concetenates letters and numbers lists
+        passwordEl.textContent = "New Password: " + genPassword(letNum);
+        console.log("letNum checked")
+    } else if (checkboxLet.checked && checkboxSym.checked) {
+        let letSym = letters.concat(symbols); // concatenates letters and symbols lists
+        passwordEl.textContent = "New Password: " + genPassword(letSym);
+        console.log("letSym checked")
+
+    } else if (checkboxNum.checked && checkboxSym.checked) {
+        let numSym = numbers.concat(symbols); // concatenates numbers and symbols lists
+        passwordEl.textContent = "New Password: " + genPassword(numSym);
+        console.log("numSym checked")
+    }
+}
+
+
+// 1. Add so that only one checkbox can be checked and still output a password
+// 2. Add a way for the user to easily copy-paste the password
+// 3. Have user input how long their password should be.
 // 5. 
 // Optional: Have them choose to include symbols/numbers
 
