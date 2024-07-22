ee41be4c5c47162fa84a24ee82c74b0cf08fbc22
Kim Petersson <kim.petersson301@gmail.com>
2024-07-22 14:53:39 +0200

    Update
    added slider for user to decide password length.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function genPassword(list) {
         return ""
     }
     let password = "";
-    for (i = 0; i < nrOfChars; i++) {
+    for (i = 0; i < passwordLength; i++) {
         let randomChar = list[genRandomNumbers(list)]; // makes random character
         password += randomChar;
     }
@@ -28,21 +28,28 @@ function outputPassword() {
     if (checkboxSym.checked) {
         allChars += symbols;
     }
-    passwordEl.textContent = genPassword(allChars);
+    passwordEl.textContent = "New password: " + genPassword(allChars);
 }
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const symbols = "~`!@#$%^&*()_-+={[}]|:;<>.?/"
 const numbers = "0123456789"
 
-let nrOfChars = 12; // How long password should be
+let passwordLength = 12; // How long password should be
 let passwordEl = document.getElementById("newpassword-el");
 let checkboxLet = document.querySelector("#letters");
 let checkboxNum = document.querySelector("#numbers");
 let checkboxSym = document.querySelector("#symbols");
 
-// 1. Add a way for the user to easily copy-paste the password
-// 2. Have user input how long their password should be.
-// 3. Add a slider for the user to decide how many characters their password is. (Check w3schools)
-// Optional: 
+let slider = document.getElementById("myRange");
+let output = document.getElementById("value");
+output.innerHTML = slider.value; // Display the default slider value
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+// 1. Add functionality for the slider so what the user inputs changes how long the password becomes.
+// 2. Add a way for the user to easily copy-paste the password (W3S?)
+// Optional: Style website more!
 
